74e434335b70b13d69d8d73fedb9bc6bece03a2c
Bruno H C Jesus <149212642+AzGrimmm@users.noreply.github.com>
2024-01-09 10:56:58 -0300

    .

--- JavaScript/Aulas Praticas/orientaçãoaobjetos/js/js.js
+++ JavaScript/Aulas Praticas/orientaçãoaobjetos/js/js.js
@@ -1,4 +1,6 @@
-function animalTag(){
+/*
+
+unction animalTag(){
     this.returnAnimal = function(animal) {
         return animal
     }
@@ -13,5 +15,26 @@ function Animal(esp,idade,peso) {
 animaltag = new animalTag()
 animal1 = new Animal("cachorro","10a","15kg")
 animal2 = new Animal("gato","8a","5kg")
+*/
 
-alert(animaltag.returnAnimal(animal1).idade)
+
+class Animal {
+    constructor(nome) {
+        this.nome = nome
+    }
+    printNome() {
+        return this.nome
+    }
+}
+
+class Cachorro extends Animal {
+    constructor(nome) {
+        super(nome)
+    }
+}
+
+
+
+cachorro = new Cachorro('Pitbul')
+
+alert(cachorro.printNome())
